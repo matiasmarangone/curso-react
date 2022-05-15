891669d413a52446b5de7cda7c01406d9ab12c56
Matias <matiasemarangone@gmail.com>
2022-05-15 18:48:34 -0300

    Add Bootsrap and Navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,43 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Navbar = () => {
+    return(<div>
+        
+
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">QUICKBUY</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarScroll">
+      <ul className="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">Inicio</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Remeras</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Sweaters</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Camperas</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Pantalones</a>
+        </li>
+       
+      </ul>
+     
+    </div>
+  </div>
+</nav>
+      
+</div>);
+}
+
+export default Navbar;
+
+
+
