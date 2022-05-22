ca3b1227e30e4e64e86162e07fc375c076d289a8
Matias <matiasemarangone@gmail.com>
2022-05-22 19:51:18 -0300

    Add ItemCount component

---
+++ src/components/ItemCount.js
@@ -0,0 +1,43 @@
+import { useEffect, useState } from 'react';
+
+
+const ItemCount = ({stock,initial,onAdd}) => {
+    const [count,setCount] = useState("0");
+
+    useEffect(() => {
+        setCount(initial);
+    },[]);
+
+    const decrement = () => {
+        if (count >= initial+1) {
+            setCount(count - 1);
+        }
+    }
+
+    const increment = () => {
+        if (count < stock) {
+            setCount(count + 1);
+        }
+    }
+
+ 
+
+    return(
+        <div>
+            <button type="button" onClick={decrement} class="btn btn-secondary">-</button>
+            
+            
+            
+            {count}
+         
+            <button type="button" onClick={increment} class="btn btn-secondary">+</button>
+            
+            {
+                stock ? <button type="button" onClick={() => onAdd(count)} class="btn btn-primary">Add to Cart</button> : <button type="button" onClick={() => onAdd(count)} class="btn btn-primary" disabled>Add to Cart</button>
+            }
+            
+        </div>
+    );
+}
+
+export default ItemCount;
--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,8 +1,16 @@
+import { alertTitleClasses } from "@mui/material";
+import ItemCount from "./ItemCount";
 
 const ItemListContainer = ({greeting}) => {
+
+    const onAdd = (quantity) => {
+        alert("Has agregado al carrito " + quantity + " items.");
+    }
+
     return(
         <div>
             {greeting}
+            <ItemCount stock={5} initial={1} onAdd={onAdd}/>
         </div>
     );
 }
